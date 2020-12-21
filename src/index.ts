879be74ae3018bfe773d5c8d764715c3d0f13e2b
Tyler Hardy <tylerthardy@gmail.com>
2020-12-21 17:46:33 -0600

    Fix DI of dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+require('dotenv').config();
+import container from './inversify.config';
+import {TYPES} from './inversify.types';
+
 console.log('Application started');
+const environment = container.get<any>(TYPES.Environment);
+console.log(`Token: ${environment.TOKEN}`);
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -2,6 +2,6 @@ import { Container } from 'inversify';
 import { TYPES } from './inversify.types';
 
 const container = new Container();
-container.bind<string>(TYPES.Environment).toConstantValue(process.env.TOKEN ?? '');
+container.bind<any>(TYPES.Environment).toConstantValue(process.env);
 
 export default container;
